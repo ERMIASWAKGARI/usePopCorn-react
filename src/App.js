2b280e473f8b3bdbe65723504182140565fedab0
ERMIASWAKGARI <ermiaswakgarisse5@gmail.com>
2023-12-15 19:38:19 +0300

    prop drilling solved

--- src/App.js
+++ src/App.js
@@ -56,19 +56,25 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search /> <NumResults movies={movies} />
+      </NavBar>
+
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
 /* STRUCTURAL COMPONENT */
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <nav className="nav-bar">
-      <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      <Logo /> {children}
     </nav>
   );
 }
@@ -109,18 +115,13 @@ function NumResults({ movies }) {
 }
 
 /* STRUCTURAL COMPONENT */
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
 /* STATEFUL COMPONENT */
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -131,7 +132,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
